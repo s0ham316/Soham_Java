334733a4398d3cb0e4366b98f987c8dfdd985cff
s0ham316 <sohamb316@gmail.com>
2022-03-29 13:54:39 +0530

    new commit

--- src/Next_10_char.java
+++ src/Next_10_char.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Next_10_char {
     public static void main(String[] args){
-
+        char c;
+        Scanner scanner=new Scanner(System.in);
+        c=scanner.next().charAt(0);
+        int n = (int) c + 10;
+        System.out.println((char)n);
     }
 }
