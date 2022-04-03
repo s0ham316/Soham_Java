bd48bbe69b73846010d0a0b965e0fddff37be181
s0ham316 <sohamb316@gmail.com>
2022-04-03 18:32:01 +0530

    Library Classes Usage and Pattern Codes

---
+++ src/ConcatString_Sum_Product.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ConcatString_Sum_Product {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int sum=0,prod=1,num=0;
+        for (int i=1;i<=10;i++){
+            System.out.println("Enter the Number: ");
+            num=scanner.nextInt();
+            sum+=num;
+            prod*=num;
+        }
+        System.out.println("Concatenated String: ");
+        System.out.println(Integer.toString(sum)+Integer.toString(prod));
+    }
+}
---
+++ src/pattern_char1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pattern_char1 {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int i,j,flag=1;
+        char c;
+        for (i=65;i<=90;i++){
+            for (j=65;j<=i;j++){
+                if (flag==1)
+                    c=(char)j;
+                else
+                    c=(char)(j+32);
+                System.out.print(c);
+            }
+            System.out.println();
+            flag*=-1;
+        }
+    }
+}
